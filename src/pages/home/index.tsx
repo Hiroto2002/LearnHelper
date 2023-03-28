cf45ed77d8999d935667ded1f4866cdcc6c1a3e1
hiroto2002 <hiroto2002h14@icloud.com>
2023-03-29 00:10:54 +0900

    fix:build error

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-import * as Post from "../../features/home/components/index";
+import React from 'react';
+import * as Post from '../../features/home/components/index';
 
- const home = () => {
-  return <div><Post.PostList></Post.PostList></div>;
+const home = () => {
+  return (
+    <div>
+      <Post.PostList></Post.PostList>
+    </div>
+  );
 };
 export default home;
