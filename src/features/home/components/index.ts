1d9f7ecbfb5cf7b4e7806a69bd4ac1988c41a118
hiroto2002 <hiroto2002h14@icloud.com>
2023-03-31 00:38:19 +0900

    fix:build error

--- src/features/home/components/index.ts
+++ src/features/home/components/index.ts
@@ -1,5 +1,5 @@
 
-import PostList from './PostList';
 import { PostButton } from './PostButton';
+import PostList from './PostList';
 import { PostModal } from './PostModal';
 export { PostList, PostButton, PostModal };
