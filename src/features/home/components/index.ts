7f6620baaa64d264683453fcf09ae178db80a4dc
hiroto2002 <hiroto2002h14@icloud.com>
2023-03-31 00:20:37 +0900

    feat:build error

--- src/features/home/components/index.ts
+++ src/features/home/components/index.ts
@@ -1,4 +1,4 @@
-import { PostList } from './Postlist';
+import { PostList } from './PostList';
 import { PostButton } from './PostButton';
 import { PostModal } from './PostModal';
-export { PostList, PostButton,PostModal };
+export { PostList, PostButton, PostModal };
