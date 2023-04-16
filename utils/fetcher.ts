107f41d4e67ddba9e417ed500396886aaf0865d9
hiroto2002 <hiroto2002h14@icloud.com>
2023-04-16 20:43:03 +0900

    fix:vercelサーバーURL

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -5,7 +5,7 @@
  * @returns {Promise<T>} API レスポンス
  */
 export const fetcher = async <T>(url: string): Promise<T> => {
-  const parsedUrl = new URL(url, "https://learn-helper.vercel.app")
+  const parsedUrl = new URL(url, "https://learn-helper.vercel.app/")
   const response = await fetch(parsedUrl.toString())
  return (await response.json()) 
 }
